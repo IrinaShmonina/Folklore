eccb0897db1a75b0a3ecabfa890095ed0896c8ee
Ирина Шмонина <shmonina@skbkontur.ru>
2020-05-24 12:08:45 +0500

    database filler

---
+++ DatabaseFiller/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Folklore.Models;
+using Newtonsoft.Json;
+
+namespace DatabaseFiller
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var uri = "https://folklore20200522003446.azurewebsites.net/api/document";
+            var client = new HttpClient();
+            var docs = JsonConvert.DeserializeObject<Document[]>(File.ReadAllText(@"C:\Git\Folklore\DatabaseFiller\docs.json"));
+            foreach (var d in docs)
+            {
+                var jsonInString = JsonConvert.SerializeObject(d);
+                await client.PostAsync(uri, new StringContent(jsonInString, Encoding.UTF8, "application/json"));
+            }
+            
+            //var resp = await client.GetAsync("https://folklore20200522003446.azurewebsites.net/api/document/all");
+            //var respStr = await resp.Content.ReadAsStringAsync();
+            //var documents = JsonConvert.DeserializeObject<Document[]>(respStr);
+            //JsonConvert.SerializeObject();
+            //Console.WriteLine(resp);
+        }
+    }
+}
